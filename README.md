cbcbbfa42cf423e040e55133b1c045b846aaa393
Mytchel <mytchel.hammond@gmail.com>
2014-05-01 02:09:49 +1200

    Rembers what page you were on when you closed it

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ showpdf
 =======
 
 A GTK pdf viewer. Is is my own pdf viewer.
+
+I'm adding some stuff to it like remembering what page you are on (working, codes bad though) and whatever else seems interesting, maybe a search.
--- showpdf.c
+++ showpdf.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <poppler.h>
 #include <gtk/gtk.h>
 #include <cairo.h>
@@ -9,22 +11,108 @@
 #define MODE_WIDTH  2
 #define MODE_FIT    3
 
+#define BACKGROUND_R 1
+#define BACKGROUND_G 1
+#define BACKGROUND_B 1
+
+#define PAGE_FILE "/home/mytchel/.config/showpdf"
+
 PopplerDocument *doc;
 PopplerPage *page;
 int pages, current = 0;
-char buffer[1024];
+gchar *file_name;
 double page_width, page_height;
 int mode = MODE_HEIGHT;
 
+void get_current_page() {
+    char line[4096];
+    char *name, *page_str;
+    FILE *page_file;
+    
+    current = 0;
+
+    page_file = fopen(PAGE_FILE, "r");
+    if (!page_file) {
+        printf("No saved page file!!!\n");
+        return;
+    }
+
+    while ((fgets(line, sizeof(char) * 4096, page_file)) != NULL) {
+        name = strtok(line, ":");
+        page_str = strtok(NULL, ":");
+
+        if (strcmp(name, file_name) == 0) {
+            current = atoi(page_str);
+            fclose(page_file);
+            return;
+        }
+    }
+
+    fclose(page_file);
+    printf("Could not find a saved page for this file\n");
+}
+
+void save_current_page() {
+    FILE *page_file, *tmp_file;
+    char line[4096];
+    char *name, *page_str;
+    int page_num;
+
+    page_file = fopen(PAGE_FILE, "r");
+    tmp_file = fopen("/tmp/showpdf", "w");
+    
+    if (!page_file || !tmp_file) {
+        printf("ERROR opening files for saving current page\n");
+        return;
+    }
+
+    while ((fgets(line, sizeof(char) * 4096, page_file)) != NULL) {
+        name = strtok(line, ":");
+        page_str = strtok(NULL, ":");
+        page_num = atoi(page_str);
+
+        if (strcmp(name, file_name) == 0) {
+            page_num = current;
+            current = -1;
+        }
+
+        fprintf(tmp_file, "%s:%i\n", name, page_num);
+    }
+
+    if (current != -1)
+        fprintf(tmp_file, "%s:%i\n", file_name, current);
+
+    fclose(page_file);
+    fclose(tmp_file);
+
+    page_file = fopen(PAGE_FILE, "w");
+    tmp_file = fopen("/tmp/showpdf", "r");
+    
+    if (!page_file || !tmp_file) {
+        printf("ERROR opening files for saving current page\n");
+        return;
+    }
+
+    while ((fgets(line, sizeof(char) * 4096, tmp_file)) != NULL)
+        fprintf(page_file, line);
+
+    fclose(page_file);
+    fclose(tmp_file);
+}
+
 void on_destroy(GtkWidget *w, gpointer data) {
     gtk_main_quit();
+    save_current_page();
 }
 
 gboolean on_expose(GtkWidget *w, GdkEventExpose *e, gpointer data) {
+    double top, left, right, bottom;
+    double scalex = 1, scaley = 1;
+    
     gtk_widget_queue_draw(w);
     gint win_width, win_height;
     gtk_window_get_size(GTK_WINDOW(w), &win_width, &win_height);
-    double scalex = 1, scaley = 1;
+   
     switch (mode) {
         case MODE_HEIGHT: scalex = scaley = win_height / page_height; break;
         case MODE_WIDTH:  scalex = scaley  = win_width  / page_width;  break;
@@ -33,8 +121,19 @@ gboolean on_expose(GtkWidget *w, GdkEventExpose *e, gpointer data) {
             scaley = win_height / page_height;
             break;
     }
+    
     cairo_t *cr = gdk_cairo_create(w->window);
     cairo_scale(cr, scalex, scaley);
+
+    // clear window.
+    top = left = 0;
+    right = win_width;
+    bottom = win_height;
+    cairo_set_source_rgb(cr, BACKGROUND_R, BACKGROUND_G, BACKGROUND_B);
+    cairo_fill_extents(cr, &top, &left, &right, &bottom);
+    cairo_fill(cr);
+    cairo_paint(cr);
+
     poppler_page_render(page, cr);
     cairo_destroy(cr);
     return FALSE;
@@ -42,29 +141,27 @@ gboolean on_expose(GtkWidget *w, GdkEventExpose *e, gpointer data) {
 
 gboolean on_keypress(GtkWidget *w, GdkEvent *e, gpointer data) {
     switch (e->key.keyval) {
-        case GDK_Page_Down: current += (current + 1 < pages) ? 1 : 0; break;
-        case GDK_Page_Up: current -= (current > 0) ? 1 : 0; break;
-        case GDK_Down: current += (current + 1 < pages) ? 1 : 0; break;
-        case GDK_Up: current -= (current > 0) ? 1 : 0; break;
-        case GDK_Left: current -= (current > 0) ? 1 : 0; break;
-        case GDK_Right: current += (current + 1 < pages) ? 1 : 0; break;
+        case GDK_k: current -= (current > 0) ? 1 : 0; break;
+        case GDK_j: current += (current + 1 < pages) ? 1 : 0; break;
         case GDK_Home: current = 0; break;
         case GDK_End: current = pages - 1; break;
         case GDK_space: current += (current + 1 < pages) ? 1 : 0; break;
         case GDK_w: mode = MODE_WIDTH; break;
         case GDK_h: mode = MODE_HEIGHT; break;
         case GDK_f: mode = MODE_FIT; break;
+        case GDK_q: on_destroy(NULL, NULL); break;
     }
+    
     g_object_unref(page);
     page = poppler_document_get_page(doc, current);
     if (!page) {
-        puts("Could not open first page of document");
+        puts("Could not open page of document");
         g_object_unref(page);
         exit(EXIT_FAILURE);
     }
+  
     poppler_page_get_size(page, &page_width, &page_height);
-    on_expose(w, NULL, NULL);
-    return FALSE;
+    on_expose(w, NULL, NULL); return FALSE;
 }
 
 int main(int argc, char **argv) {
@@ -72,8 +169,10 @@ int main(int argc, char **argv) {
         printf("Usage: %s FILE\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
     gtk_init(&argc, &argv);
     GError *err = NULL;
+   
     gchar *filename = argv[1], *absolute;
     if (g_path_is_absolute(filename)) {
         absolute = g_strdup (filename);
@@ -82,26 +181,35 @@ int main(int argc, char **argv) {
         absolute = g_build_filename (dir, filename, (gchar *) 0);
         free (dir);
     }
+
+    file_name = g_strdup(absolute);
+
     gchar *uri = g_filename_to_uri (absolute, NULL, &err);
     free (absolute);
     if (uri == NULL) {
         printf("poppler fail: %s\n", err->message);
         exit(EXIT_FAILURE);
     }
+   
     doc = poppler_document_new_from_file(uri, NULL, &err);
     if (!doc) {
         puts(err->message);
         g_object_unref(doc);
         exit(EXIT_FAILURE);
     }
+    
+    get_current_page();
+   
     page = poppler_document_get_page(doc, current);
     if (!page) {
         puts("Could not open first page of document");
         g_object_unref(page);
         exit(EXIT_FAILURE);
     }
+
     poppler_page_get_size(page, &page_width, &page_height);
     pages = poppler_document_get_n_pages(doc);
+
     GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     g_signal_connect(G_OBJECT(win), "destroy", G_CALLBACK(on_destroy), NULL);
     g_signal_connect(G_OBJECT(win), "expose-event", G_CALLBACK(on_expose), NULL);
